56e21209e2c723c49e0d58f4d8b512d065a81b49
Kaylei Burke <kaylei.burke@gmail.com>
2019-07-10 20:32:54 -0500

    Format the PaymentSpring transaction data in the Payments controller GET method

--- app/controllers/api/v1/payments_controller.rb
+++ app/controllers/api/v1/payments_controller.rb
@@ -1,10 +1,20 @@
 class Api::V1::PaymentsController < ApplicationController
   before_action :authenticate_user!
   include Api::V1::Concerns::PaymentspringApiConcern
+  include ActionView::Helpers::NumberHelper
+  require 'date'
 
   def index
-    parsed_response = call_api('api/v1/transactions', {}, params, "GET")
-    render json: parsed_response
+    response = call_api('api/v1/transactions', {}, { limit: 100, offset: 0 }, "GET")
+
+    if response["list"]
+      response = response["list"].sort_by { |h| h["created_at"] }.group_by { |h| Date.parse h["created_at"] }.map do |k,v|
+        amount = v.map {|h1| h1["amount_settled"]}.inject(:+)/100.0
+        [k.strftime("%m/%d"), amount]
+      end
+    end
+
+    render json: response
   end
 
   def create
@@ -15,6 +25,7 @@ class Api::V1::PaymentsController < ApplicationController
     }.to_json
 
     parsed_response = call_api('api/v1/charge', body, {}, "POST")
+
     render json: parsed_response
   end
 end
