bdddc109eed3ded660fa5749be81fb32f4b4e8f1
Kaylei Burke <kaylei.burke@gmail.com>
2019-07-11 08:24:04 -0500

    Return total results and total donations in the index method of the payments controller

--- app/controllers/api/v1/payments_controller.rb
+++ app/controllers/api/v1/payments_controller.rb
@@ -1,17 +1,32 @@
 class Api::V1::PaymentsController < ApplicationController
   before_action :authenticate_user!
   include Api::V1::Concerns::PaymentspringApiConcern
-  include ActionView::Helpers::NumberHelper
+  include ActionView::Helpers::DateHelper
   require 'date'
 
   def index
     response = call_api('api/v1/transactions', {}, { limit: 100, offset: 0 }, "GET")
 
+    total_results = 0
+    total_donations = 0
+
     if response["list"]
+      total_results = response["meta"]["total_results"]
       response = response["list"].sort_by { |h| h["created_at"] }.group_by { |h| Date.parse((Time.parse h["created_at"]).getlocal.to_s) }.map do |k,v|
-        amount = v.map {|h1| h1["amount_settled"]}.inject(:+)/100.0
+        amount = (v.map {|h1| h1["amount_settled"]}.sum)/100.0
+        total_donations += amount
         [k.strftime("%m/%d"), amount]
       end
+
+      start_date = Date.parse(response[0][0])
+      end_date = Date.parse(response[response.size - 1][0])
+
+      response = {
+          list: response,
+          count: total_results,
+          total: total_donations.round(2),
+          timespan: distance_of_time_in_words(start_date, end_date)
+      }.to_json
     end
 
     render json: response
